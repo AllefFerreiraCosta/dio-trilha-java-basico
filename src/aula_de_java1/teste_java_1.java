8d8ec2dbc310c64cb46d394ea6540bcd23eeeaa9
allef <allef.f.costa3@gmail.com>
2024-06-07 23:20:54 -0300

    modificações

---
+++ src/aula_de_java1/teste_java_1.java
@@ -0,0 +1,12 @@
+package aula_de_java1;
+
+public class teste_java_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numeroChamada = 23;
+		
+		System.out.println(numeroChamada);	}
+
+}
